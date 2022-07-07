4ce9868cfcf6cde088c89ee340d7c7d0b40380e6
HappyEXS <janek.jedrzejewski@wp.pl>
2022-07-07 09:22:23 +0200

    update main and gitignore

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from account import Account
+from market import Market
+
 def main():
-    print("App is running")
+    acc = Account("janek")
+    print(acc)
+    market = Market()
+    market.update()
+    print(market)
+    print(market.getLivePrices())
 
 if __name__ == '__main__':
     main()
